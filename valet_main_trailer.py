60a9a3ed3ead6263d09782e5ded266d9865d42fa
Jason P <jpatel@nuhabit.ai>
2023-10-27 16:55:13 -0400

    Add trailer

---
+++ valet_main_trailer.py
@@ -0,0 +1,88 @@
+import pygame
+import math
+from math import pi
+# from valrobots.ackermann import RobotAckermann
+from valet.robots.trailer import RobotTrailer
+from valet.environment import Envir
+from valet.lattice import Lattice
+import time
+
+
+pygame.init()
+# start=(100,450,0)
+# start=(100,100,0)
+# start=(100,250,0)
+# start=(100,100,0)
+# start=(550, 550,0)
+# start=(100,100,0)
+# start=(550, 550,0)
+
+start=(150,100,0)
+
+# goal=(550, 650,0)
+# goal=(400, 500,0)
+# goal=(575, 650,0)
+
+goal=(250, 100,0)
+goal=(600, 620,0)
+dims=(800,800)
+running=True
+
+robot=RobotTrailer(start,
+            (r"./valet/ackermann2.png",r"./valet/trailer.png"),
+            # 35,goal)
+            .1*3779.52,goal)
+env=Envir(dims,robot,goal)
+
+env.obstacles:list[pygame.Rect] = [
+            # pygame.Rect(100, 100, 50, 50),
+            # pygame.Rect(400, 400, 100, 100),
+            # pygame.Rect(400, 600, 50, 50),
+            pygame.Rect(300, 200, 200, 200),  
+            pygame.Rect(200, 600, 200, 40),
+            pygame.Rect(0, 650, 800, 160),
+            pygame.Rect(650, 600, 150, 40),
+        ]
+
+
+
+
+
+env.map.fill(env.black)
+env.draw_obstacles()
+env.draw_goal()
+robot.draw(env.map)
+pygame.display.update()
+time.sleep(2)
+
+dt=0
+lastime=pygame.time.get_ticks()
+lattice = Lattice(start,goal,env.obstacles,robot,env.map,env.write_text_info)
+lastState = lattice.search()
+time.sleep(2)
+
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            running=False
+    #     robot.move(event)
+    dt = (pygame.time.get_ticks()-lastime)/1000
+    lastime=pygame.time.get_ticks()
+    pygame.display.update()
+    
+    #robot.move()
+    #lattice.add_neighbors(robot.plan())
+    #lattice.plan()
+    nextMove = lattice.step2()
+    robot.drive(nextMove)
+    lattice.currentState=nextMove
+    time.sleep(.25)
+    env.map.fill(env.black)
+    env.draw_obstacles()
+    env.draw_goal()
+
+    robot.draw(env.map)
+    env.write_info()
+    
